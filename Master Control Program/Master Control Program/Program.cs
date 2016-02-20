f53009d8e878b128816bf7a2ccd208feb96e50fd
z0wb13 <z0wb13@gmail.com>
2016-02-20 10:10:01 -0500

    testing commits

---
+++ Master Control Program/Master Control Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_Control_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+
+
+
+
+
+        class Elevator{}
+
+        class Buttons { }
+        class Display { }
+
+        class Scheduler { }
+
+
+
+
+
+
+
+    }
+
+    
+
+
+}
